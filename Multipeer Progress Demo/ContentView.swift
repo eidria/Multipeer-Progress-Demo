030fc0ff49ac042e4873587a3ae91b969340841c
Dan Galbraith <dan@eidria.com>
2024-08-24 14:23:52 -0700

    Initial Commit

---
+++ Multipeer Progress Demo/ContentView.swift
@@ -0,0 +1,25 @@
+////    ContentView.swift
+//    Multipeer Progress Demo
+//
+//    Created by Dan Galbraith on 8/24/24.
+//
+//    Copyright © 2024 Eidria Inc. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multipeer Progress Demo/Multipeer_Progress_DemoApp.swift
@@ -0,0 +1,18 @@
+////    Multipeer_Progress_DemoApp.swift
+//    Multipeer Progress Demo
+//
+//    Created by Dan Galbraith on 8/24/24.
+//
+//    Copyright © 2024 Eidria Inc. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct Multipeer_Progress_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
